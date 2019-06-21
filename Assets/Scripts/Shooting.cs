6ac1cf3051790ebc548cd253948bc163c5012f79
zacharytafe <36681233+zacharytafe@users.noreply.github.com>
2019-06-21 16:00:56 +1000

    Added menu, score list and edited shooting.

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -5,35 +5,61 @@ using UnityEngine.UI;
 
 public class Shooting : MonoBehaviour
 {
+    public float speed;
     public GameObject bullet;
     public GameObject endGame;
-    public float speed;
-    public float ballCount = 100;
+    public AudioSource throwBall;
 
+    public float timeBeforeEnd = 10;
+    public float ballCount = 20;
     public Text ballLimit;
+
+    private bool canShoot;
+    private IEnumerator coroutine;
 
     void Start()
     {
-        // ballCount = 100;
         ballLimit.text = ballCount + ("");
+
+        canShoot = true;
+    }
+
+   
+    void Update()
+    {
+        if (canShoot)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                GameObject insBullet = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
+                Rigidbody insBulletRig = insBullet.GetComponent<Rigidbody>();
+                insBulletRig.AddForce(transform.forward * speed);
+
+                ballCount -= 1;
+                if (ballCount <= 0)
+                {
+                    StartCoroutine(Timer());
+                    canShoot = false;
+                }
+
+                throwBall.Play();
+            }
+        }
+
+        ballLimit.text = ballCount + ("");
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(7);
+        GameOver();
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            GameObject insBullet = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
-            Rigidbody insBulletRig = insBullet.GetComponent<Rigidbody>();
-            insBulletRig.AddForce(transform.forward * speed);
-
-            ballCount -= 1;
-            if (ballCount <= 0)
-            {
-                endGame.SetActive(true);
-                Time.timeScale = 0;
-            }
-        }
+    void GameOver()
+    {       
+        endGame.SetActive(true);
+        Time.timeScale = 0;
+        //Destroy(ballLimit);
     }
 }
